73d4b4a5b4408683c731475b711e1f5988fb16f6
MarkPelf <markpelf@markpelf.com>
2022-07-25 12:42:00 +0200

    Commit 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Test01");
+            Console.WriteLine("Test02");
             Console.WriteLine("Hello World!");
         }
     }
